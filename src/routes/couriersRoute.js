b811dafa8dfae5240cd1acb0d9f3b1ab747c0236
Kiril <fanky.yahoo@gmail.com>
2025-04-18 10:43:13 +0300

    test commit with small changes

---
+++ src/routes/couriersRoute.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import expressAsyncHandler from 'express-async-handler';
+import jwt from 'jsonwebtoken';
+import config from 'config';
+import { validator } from '../middleware/validation.js';
+import accountsService from "../service/AccountsService.js";
+import dotenv from "dotenv";
+import expressAsyncHandler from "express-async-handler";
+import { authenticate, authorize } from "../middleware/auth.js";
+import { validator } from "../middleware/validation.js";
+import { schemaLogin } from "../validation/AccountSchemas.js"
+import {schemaCourierAccount} from "../validation/CourierSchemas.js"
+
+dotenv.config();
+
+const couriersRoute = express.Router();
+
+//addCourierAccount!
+couriersRoute.post("/post", validator(schemaCourierAccount), expressAsyncHandler(async (req, res) => {
+    const courier = await courierService.addAccount(req.body);
+    res.status(201).send(courier);
+}));
+
+//login
+couriersRoute.post("/login", validator(schemaLogin), expressAsyncHandler(async (req, res) => {
+    const token = await courierService.login(req.body);
+    res.status(201).send(token);
+}));
+export default couriersRoute;
---
+++ src/validation/CourierSchemas.js
@@ -0,0 +1,31 @@
+import Joi from "joi";
+import config from "config";
+import { joiPasswordExtendCore } from "joi-password";
+import { passwordSchema, nameSchema, phoneSchema } from "./AccountSchemas.js";
+const joiPassword = Joi.extend(joiPasswordExtendCore);
+const schemaUser = Joi.string().email().required();
+
+export const schemaCourierAccount = Joi.object({
+    email: schemaUser,
+    password: passwordSchema,
+    name: nameSchema,
+    phone: phoneSchema
+});
+
+export const schemaGetAccount = Joi.object({
+    email: schemaUser,
+});
+
+const nameSchema = Joi.string()
+    .pattern(/^\S+\s\S+$/)
+    .required();
+
+
+
+export const schemaAccount = Joi.object({
+    email: schemaUser,
+    username: nameSchema,
+    password: passwordSchema,
+    role: Joi.string().valid(...config.get("accounting.roles")),
+});
+
